f3a1e79d1aecec3c2998165cfe7947d1aafa196c
mitogh <mitogh@gmail.com>
2016-03-21 21:23:07 -0600

    Fix CI issues

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -2,34 +2,83 @@
 
 use Leean\Endpoints\Filters;
 
+/**
+ * Class that creates the default behavior used to register a new endpoint and
+ * also the same logic that is used across all lean PHP modules.
+ *
+ * @package Leean;
+ */
 abstract class Endpoint {
 
-	protected $ENDPOINT = '/leean';
-	protected $HTTP_VERB = 'GET';
+	/**
+	 * The path of the endpoint to be created.
+	 *
+	 * @var string
+	 */
+	protected $endpoint = '/leean';
 
+	/**
+	 * HTTP Verb used on the API.
+	 *
+	 * @var string
+	 */
+	protected $http_verb = 'GET';
+
+	/**
+	 * Register the endpoint on the WP Rest API and initializes the variables.
+	 *
+	 * @since 0.1.0
+	 */
 	public function create() {
 		$this->set_variables();
-		add_action( 'rest_api_init', [$this, 'register']);
+		add_action( 'rest_api_init', [ $this, 'register' ] );
 	}
 
-	private function set_variables(){
-		$this->namespace = apply_filters( Filters::API_NAMESPACE, 'leean', $this->ENDPOINT );
-		$this->version = apply_filters( Filters::API_VERSION, 'v1', $this->ENDPOINT );
+	/**
+	 * Variables.
+	 *
+	 * @since 0.1.0
+	 */
+	private function set_variables() {
+		$this->namespace = apply_filters( Filters::API_NAMESPACE, 'leean', $this->endpoint );
+		$this->version = apply_filters( Filters::API_VERSION, 'v1', $this->endpoint );
 		$this->options = [
-			'methods' => $this->HTTP_VERB,
-			'callback' => [ $this, 'endpointCallback' ],
-			'args' => $this->endpointArgs(),
+			'methods' => $this->http_verb,
+			'callback' => [ $this, 'endpoint_callback' ],
+			'args' => $this->endpoint_args(),
 		];
 	}
 
-	public function register(){
+	/**
+	 * Callback called from rest_api_init action that creates the new endpoint.
+	 *
+	 * @since 0.1.0
+	 */
+	public function register() {
 		register_rest_route(
 			$this->namespace . '/' . $this->version,
-			$this->ENDPOINT,
+			$this->endpoint,
 			$this->options
 		);
 	}
 
-	abstract public function endpointCallback(\WP_REST_Request $request);
-	abstract public function endpointArgs();
+	/**
+	 * This is the callback where all the logic of the endpoint is handled here you
+	 * recieve a $request param that can be used to handle queries or any other
+	 * operation to generate the endpoint.
+	 *
+	 * @param \WP_REST_Request $request Contains data from the request.
+	 */
+	abstract public function endpoint_callback( \WP_REST_Request $request );
+
+	/**
+	 * Arguments that the endpoint can recieve in this way we can customize this
+	 * on the class that extends from this one.
+	 *
+	 * The function must return an array with the required params.
+	 *
+	 * @since 0.1.0
+	 * @return Array
+	 */
+	abstract public function endpoint_args();
 }
--- src/Endpoints/Filters.php
+++ src/Endpoints/Filters.php
@@ -1,5 +1,10 @@
 <?php namespace Leean\Endpoints;
 
+/**
+ * Defines the filters used on the Endpoint class.
+ *
+ * @package Leean/Endpoints
+ */
 class Filters {
 	const API_NAMESPACE = 'ln_endpoints_api_namespace';
 	const API_VERSION = 'ln_endpoints_api_version';
